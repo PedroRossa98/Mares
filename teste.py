9940e23c2e74c6faca848945ea1517801911ae68
PedroRossa98 <pedro.rossa@tecnico.ulisboa.pt>
2022-01-13 20:38:55 +0000

    New projected

--- teste.py
+++ teste.py
@@ -10,6 +10,7 @@ config = configparser.ConfigParser()
 config.read('pendulum.ini')
 header_pendulum= ['Samples number','Period [s]','g [m/s^2]','Velocity [m/s]','Temperature [ºC]']
 
+#https://stackoverflow.com/questions/54944524/how-to-write-csv-file-into-specific-folder
 if __name__ == "__main__":
     name_file=datetime.now()
     name_file = "./"+str(config['DEFAULT']['FOLDER'])+"/"+name_file.strftime("%Y-%m-%d %H:%M:%S")+".csv"
