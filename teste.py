a97cf5482c307f69305922a49f4e7e5e44927588
PedroRossa98 <pedro.rossa@tecnico.ulisboa.pt>
2022-01-13 20:28:13 +0000

    New projected

---
+++ teste.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+from email import header
+import serial
+import json
+import re
+import csv
+import configparser
+import experiment_details as exp
+config = configparser.ConfigParser()
+config.read('pendulum.ini')
+header_pendulum= ['Samples number','Period [s]','g [m/s^2]','Velocity [m/s]','Temperature [ºC]']
+
+if __name__ == "__main__":
+    name_file=datetime.now()
+    name_file = "/"+str(config['DEFAULT']['FOLDER'])+"/"+name_file.strftime("%Y-%m-%d %H:%M:%S")+".csv"
+    print(name_file)
+    with open(name_file, mode='w') as csv_file:
+        writer = csv.writer(csv_file, delimiter=',')
+        writer.writerow(header_pendulum)
