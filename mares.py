41804b286dfd47b143903b2891ba20ae946d71d6
PedroRossa98 <pedro.rossa@tecnico.ulisboa.pt>
2022-01-13 20:24:09 +0000

    New projected

--- mares.py
+++ mares.py
@@ -31,14 +31,16 @@ def try_to_lock_experiment(serial_port):
         if match.group("exp_state") == "STOPED":
             return True
         else:
-            print("STATE OF MACHINE DIF OF STOPED")
+            if (dbuging == "on"):
+                print("STATE OF MACHINE DIF OF STOPED")
             if do_stop():
                 return True
             else:
                 return False
     else:
         #LOG INFO
-        print("PIC NOT FOUND ON THE SERIAL PORT")
+        if (dbuging == "on"):
+            print("PIC NOT FOUND ON THE SERIAL PORT")
         return False
 
 def serial_lock():
@@ -61,17 +63,18 @@ def serial_lock():
 
 def do_start() :
     global serial_port
-
-    print("A tentar comecar a experiencia\n")
+    if (dbuging == "on"):
+        print("Trying to start the experiment\n")
     cmd = "str\r"
     cmd = cmd.encode(encoding='ascii')
     serial_port.reset_input_buffer()
     serial_port.write(cmd)
     while True :
         pic_message = serial_port.read_until(b'\r')
-        print("MENSAGEM DO PIC A CONFIRMAR STROK:\n")
-        print(pic_message.decode(encoding='ascii'))
-        print("\-------- --------/\n")
+        if (dbuging == "on"):
+            print("MESSAGE FROM THE PIC TO CONFIRM STROK: \n")
+            print(pic_message.decode(encoding='ascii'))
+            print("\-------- --------/\n")
         if "STROK" in pic_message.decode(encoding='ascii') :
             return True
         elif re.search(r"(STOPED|CONFIGURED|RESETED){1}$",pic_message.decode(encoding='ascii')) != None:
@@ -84,8 +87,8 @@ def do_start() :
 
 def do_stop() :
     global serial_port
-    
-    print("Try to stop the experiment\n")
+    if (dbuging == "on"):
+        print("Try to stop the experiment\n")
     cmd = "stp\r"
     cmd = cmd.encode(encoding='ascii')
     serial_port.reset_input_buffer()
@@ -94,10 +97,11 @@ def do_stop() :
     while True :
         try:
             pic_message = serial_port.read_until(b'\r')
-            print("MENSAGEM DO PIC A CONFIRMAR STPOK:\n")
-            print(pic_message.decode(encoding='ascii'))
-            print("\-------- ! --------/\n")
-            print ( pic_message.decode(encoding='ascii').split("\t"))
+            if (dbuging == "on"):
+                print("MESSAGE FROM THE PIC TO CONFIRM STPOK: \n")
+                print(pic_message.decode(encoding='ascii'))
+                print("\-------- ! --------/\n")
+                print ( pic_message.decode(encoding='ascii').split("\t"))
         except:
             pass
         if "STPOK" in pic_message.decode(encoding='ascii') :
@@ -107,7 +111,8 @@ def do_stop() :
         #     pass
         elif len(pic_message.decode(encoding='ascii').split("\t")) == 3 and  pic_message.decode(encoding='ascii').split("\t")[2] in ["CONFIGURED\r","RESETED\r"] :
         # elif re.search(r"(CONFIGURED|RESETED){1}$",pic_message.decode(encoding='ascii')) != None :
-            print("There is garbage in the serial port try the command again!")
+            if (dbuging == "on"):
+                print("There is garbage in the serial port try the command again!")
             serial_port.reset_input_buffer()
             serial_port.write(cmd)
         # Maybe create a counter to give a time out if the pic is not working correct
@@ -117,17 +122,18 @@ def do_stop() :
 
 def do_reset() :
     global serial_port
-
-    print("A tentar fazer reset da experiencia\n")
+    if (dbuging == "on"):
+        print("Trying to reset the experience \n")
     cmd = "rst\r"
     cmd = cmd.encode(encoding='ascii')
     serial_port.reset_input_buffer()
     serial_port.write(cmd)
     while True :
         pic_message = serial_port.read_until(b'\r')
-        print("MENSAGEM DO PIC A CONFIRMAR RSTOK:\n")
-        print(pic_message.decode(encoding='ascii'))
-        print("\-------- --------/\n")
+        if (dbuging == "on"):
+            print("MESSAGE FROM THE PIC TO CONFIRM RSTOK: \n")
+            print(pic_message.decode(encoding='ascii'))
+            print("\-------- --------/\n")
         if "RSTOK" in pic_message.decode(encoding='ascii') :
             return True
         elif re.search(r"(STOPED|CONFIGURED){1}$",pic_message.decode(encoding='ascii')) != None :
@@ -146,12 +152,14 @@ def do_config() :
     #interessam
     serial_port.reset_input_buffer()
     serial_port.write(cmd)
-    print("A tentar configurar experiência")
+    if (dbuging == "on"):
+        print("TRYING TO CFG THE PIC")
     while True :
         pic_message = serial_port.read_until(b'\r')
-        print("MENSAGEM DO PIC DE CONFIGURACAO:\n")
-        print(pic_message.decode(encoding='ascii'))
-        print("\-------- --------/\n")
+        if (dbuging == "on"):
+            print("CONFIGURATION PIC MESSAGE:\n")
+            print(pic_message.decode(encoding='ascii'))
+            print("\-------- --------/\n")
         if "CFG" in pic_message.decode(encoding='ascii') :
             pic_message = pic_message.decode(encoding='ascii')
             #Remover os primeiros 4 caracteres para tirar o "CFG\t" 
@@ -162,9 +170,10 @@ def do_config() :
             return -1,False
     status_confirmation = serial_port.read_until(b'\r')
     status_confirmation = status_confirmation.decode(encoding='ascii')
-    print("MENSAGEM DO PIC A CONFIRMAR CFGOK:\n")
-    print(status_confirmation)
-    print("\-------- --------/\n")
+    if (dbuging == "on"):
+        print("PIC MESSAGE TO CONFIRM CFGOK: \n")
+        print(status_confirmation)
+        print("\-------- --------/\n")
     if "CFGOK" in status_confirmation:
         return pic_message, True
     else:
@@ -206,8 +215,9 @@ if __name__ == "__main__":
         if ok == True:
             do_start()
             name_file=datetime.now()
-            print("\\"+str(config['DEFAULT']['FOLDER'])+"\\"+name_file.strftime("%Y-%m-%d %H:%M:%S")+".csv")
-            with open("\\"+str(config['DEFAULT']['FOLDER'])+"\\"+name_file.strftime("%Y-%m-%d %H:%M:%S")+".csv", mode='w') as csv_file:
+            name_file = "/"+str(config['DEFAULT']['FOLDER'])+"/"+name_file.strftime("%Y-%m-%d %H:%M:%S")+".csv"
+            print(name_file)
+            with open(name_file, mode='wb') as csv_file:
                 writer = csv.writer(csv_file, delimiter=',')
                 writer.writerow(header_pendulum)
                 while True:
